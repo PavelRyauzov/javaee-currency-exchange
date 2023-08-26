3d025f39700400db58eb3e4c7b559d1a6798a3f4
Pavel Ryauzov <pavel.ryauzov@gmail.com>
2023-08-26 17:06:57 +0300

    base servlets created

---
+++ src/main/java/ExchangeRateServlet.java
@@ -0,0 +1,28 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "ExchangeRateServlet", value = "/exchangeRate/*")
+public class ExchangeRateServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (!req.getMethod().equals("PATCH")) {
+            super.service(req, resp);
+        }
+
+        this.doPatch(req, resp);
+    }
+
+    protected void doPatch(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
+}
