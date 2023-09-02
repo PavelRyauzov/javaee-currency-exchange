0416aaf380f96dcca9b34882a2e6768a4a9bbb12
Pavel Ryauzov <pavel.ryauzov@gmail.com>
2023-09-02 22:57:37 +0300

    fixed the logic of returning results on the CurrencyDao

--- src/main/java/dao/impl/CurrencyDaoImpl.java
+++ src/main/java/dao/impl/CurrencyDaoImpl.java
@@ -5,6 +5,7 @@ import model.Currency;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,11 +18,10 @@ public class CurrencyDaoImpl implements CurrencyDao {
 
     @Override
     public List<Currency> findAll() {
-        List<Currency> currencies = new ArrayList<>();
-
         try(ResultSet resultSet = connection.createStatement().executeQuery(
                 "SELECT * FROM Currencies;"
         )) {
+            List<Currency> currencies = new ArrayList<>();
             while (resultSet.next()) {
                 currencies.add(new Currency(
                         resultSet.getInt("ID"),
@@ -30,23 +30,22 @@ public class CurrencyDaoImpl implements CurrencyDao {
                         resultSet.getString("Sign")
                 ));
             }
+            return currencies;
         } catch (SQLException e) {
             //todo add logging
             e.printStackTrace();
+            return Collections.emptyList();
         }
-
-        return currencies;
     }
 
     @Override
     public Optional<Currency> findByCode(String code) {
-        Currency currency = null;
-
         try(PreparedStatement statement = connection.prepareStatement(
                 "SELECT * FROM Currencies WHERE Code = ?"
         )) {
            statement.setString(1, code);
            try(ResultSet resultSet = statement.executeQuery()) {
+               Currency currency = null;
                if (resultSet.next()) {
                    currency = new Currency(
                            resultSet.getInt("ID"),
@@ -55,12 +54,12 @@ public class CurrencyDaoImpl implements CurrencyDao {
                            resultSet.getString("Sign")
                    );
                }
+               return Optional.ofNullable(currency);
            }
         } catch (SQLException e) {
             //todo add logging
             e.printStackTrace();
+            return Optional.empty();
         }
-
-        return Optional.ofNullable(currency);
     }
 }
