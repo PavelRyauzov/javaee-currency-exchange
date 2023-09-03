ef2df767f8495ea027960f5abb50a3f9832a414e
Pavel Ryauzov <pavel.ryauzov@gmail.com>
2023-09-04 00:00:52 +0300

    rollback to sqllite config from properties file

--- src/main/java/listener/ApplicationServletContextListener.java
+++ src/main/java/listener/ApplicationServletContextListener.java
@@ -2,7 +2,6 @@ package listener;
 
 import dao.impl.CurrencyDaoImpl;
 import db.SqliteDataSource;
-import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
@@ -11,17 +10,11 @@ import service.impl.CurrencyServiceImpl;
 @WebListener
 public class ApplicationServletContextListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent contextEvent) {
-        ServletContext context = contextEvent.getServletContext();
-        try {
-            context.setAttribute("currencyService",
-                    new CurrencyServiceImpl(
-                            new CurrencyDaoImpl(
-                                    new SqliteDataSource()
-                            )
-                    )
-            );
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to initialize application", e);
-        }
+        contextEvent.getServletContext()
+                .setAttribute("currencyService", new CurrencyServiceImpl(
+                        new CurrencyDaoImpl(
+                                new SqliteDataSource()
+                        )
+                ));
     }
 }
