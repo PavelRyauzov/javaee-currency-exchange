ae136fec78856621fb70c9caf841fc7525af6d35
Pavel Ryauzov <pavel.ryauzov@gmail.com>
2023-09-15 00:39:21 +0300

    exchange rate model fix

--- src/main/java/model/ExchangeRate.java
+++ src/main/java/model/ExchangeRate.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 @Value
 public class ExchangeRate {
     int id;
-    int baseCurrencyId;
-    int targetCurrencyId;
+    Currency baseCurrencyId;
+    Currency targetCurrencyId;
     BigDecimal rate;
 }
