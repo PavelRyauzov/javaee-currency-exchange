15041ba1c8e9852409f4284819d8f593966b6cf9
Pavel Ryauzov <pavel.ryauzov@gmail.com>
2023-09-15 00:46:34 +0300

    exchange rate model fields naming fix

--- src/main/java/model/ExchangeRate.java
+++ src/main/java/model/ExchangeRate.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 @Value
 public class ExchangeRate {
     int id;
-    Currency baseCurrencyId;
-    Currency targetCurrencyId;
+    Currency baseCurrency;
+    Currency targetCurrency;
     BigDecimal rate;
 }
